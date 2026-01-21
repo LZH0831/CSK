8d9072e9d21961bdfc392591cfdf447fe2d85295
linzhihui <2935959437@qq.com>
2026-01-22 01:16:08 +0800

    tijiao

--- 版本3/main.m
+++ 版本3/main.m
@@ -9,7 +9,8 @@ total=zeros(2,3,25);
 
 for l=1:length(L_arr)
     L=L_arr(l);
-    for beta=1:length(beta_arr)
+    for b=1:length(beta_arr)
+        beta=beta_arr(b);
         M=floor(log2(beta));
         for dB=0:1:24
             disp(dB);
@@ -23,16 +24,21 @@ for l=1:length(L_arr)
             [Bits,Symbols0]=Transmitter(M,beta,Block_Num,C);
             Symbols1=Channel(Symbols0,L,SNR,M,beta,cur_alpha);
             Bitsre=Receiver(M,Block_Num,C,Symbols1);
-            ratio(1,dB+1)=sum(Bits~=Bitsre)/((Block_Num-1)*M);
-            total(1,dB+1)=total(1,dB+1)+ratio(1,dB+1);
+            ratio(l,b,dB+1)=sum(Bits~=Bitsre)/((Block_Num-1)*M);
+            total(l,b,dB+1)=total(l,b,dB+1)+ratio(l,b,dB+1);
             end
         end
-        total=total/Frame_Num;
     end
 end
+total=total/Frame_Num;
 figure();
 box on; hold on;
-plot(0:1:24, total(1,:), 'bo-');
+markers={'bo-','rs-','k^-'};
+for i=1:2
+    for j=1:3
+        plot(0:1:24,squeeze(total(i,j,:)),markers{j});
+    end
+end
 set(gca, 'Yscale', 'log');
 ylim([1e-6 1]);          
 xlabel('Eb/N0 (dB)');
